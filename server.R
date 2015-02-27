6cf3ad9ad808662fe4b31386cc940d42c6083e56
Famibelle Médhi <medhi.famibelle@orange.com>
2015-02-27 19:42:49 +0100

    Bug fixes and dedicated colors for Post types

--- server.R
+++ server.R
@@ -2,6 +2,7 @@ library(googleVis)
 library(xlsx)
 library(plyr)
 library(lubridate)
+library(xts)
 
 # By default, the file size limit is 5MB. It can be changed by
 # setting this option. Here we'll raise limit to 9MB.
@@ -10,10 +11,9 @@ options(shiny.maxRequestSize = 9*1024^2)
 shinyServer(function(input, output) {
     reactive_Facebook <- reactive({
         validate(
-            need(input$file1 != "", "Auncun fichier n'a été uploadé pour le moment")
+            need(input$file1 != "", "Aucun fichier n'a été uploadé pour le moment")
         )
-        
-        
+                
         # input$file1 will be NULL initially. After the user selects
         # and uploads a file, it will be a data frame with 'name',
         # 'size', 'type', and 'datapath' columns. The 'datapath'
@@ -25,29 +25,45 @@ shinyServer(function(input, output) {
 
         facebook.insight <- read.xlsx(
             inFile$datapath, 
-#             sheetIndex = input$sheetIndex,
             sheetIndex = 1,
-            stringsAsFactors=FALSE
-            )
-        facebook.insight <- facebook.insight[-1,]
-#         class(facebook.insight$Lifetime.Post.Total.Reach) <- "numeric"
-#         class(facebook.insight$Lifetime.Post.organic.reach) <- "numeric"
-#         class(facebook.insight$Lifetime.Engaged.Users) <- "numeric"
-#         class(facebook.insight$Posted) <- "numeric"        
-        cat("reactive_Facebook")
-        cat(paste(sapply(Facebook_OFR_Post,class), sep="\n"))
-        return(facebook.insight)
+            startRow = 3, 
+            header = FALSE
+        )
+    
+        facebook.insight.colTitle <- read.xlsx2(
+            inFile$datapath, 
+            sheetIndex = 1,
+            startRow = 1, 
+            endRow = 2,
+            stringsAsFactors = FALSE
+        )
+            
+    colnames(facebook.insight) <- colnames(facebook.insight.colTitle)
+    
+    # erase uploaded file once all the data have been uploaded
+    unlink(inFile$datapath)
+    
+    return(facebook.insight)
     })
 
+
+############################ Chooser ############################
+    output$selection <- renderDygraph(
+        withProgress(message = 'Extraction et analyse du fichier en cours ',
+                     detail = 'Patience...', value = 0, {
+                         facebook.insight <- reactive_Facebook()
+                     })
+        
+#         facebook.insight.TimeSerie <- xts(facebook.insight[,-7], order.by = facebook.insight$Posted)
+#         dygraph(facebook.insight.TimeSerie[,input$mychooser])
+    )
+    
 ############################ insigth table ############################
     output$contents <- renderGvis({
         withProgress(message = 'Extraction et analyse du fichier...',
                      detail = 'Patience...', value = 0, {
                          Facebook_OFR_Post <- reactive_Facebook()
                      })
-        
-        class(Facebook_OFR_Post$Posted) <- "numeric"
-        Facebook_OFR_Post$Posted <- as.Date(Facebook_OFR_Post$Posted, origin = "1900-01-01")
         
         InsightTable <- gvisTable(
             Facebook_OFR_Post,
@@ -64,27 +80,25 @@ shinyServer(function(input, output) {
                          Facebook_OFR_Post <- reactive_Facebook()
                      })
         
-        class(Facebook_OFR_Post$Lifetime.Post.organic.reach)  <- "numeric"
-        class(Facebook_OFR_Post$Lifetime.Post.Total.Reach)  <- "numeric"
-        class(Facebook_OFR_Post$Lifetime.Engaged.Users)  <- "numeric"
-                
+
         #Coerce Posted to class Date
-        class(Facebook_OFR_Post$Posted) <- "numeric"
-        Facebook_OFR_Post$Posted <- as.Date(Facebook_OFR_Post$Posted, origin = "1900-01-01")-2 #In the Excel 1900 date system, the Excel serial date number 1 corresponds to January 1, 1900 A.D. In the Excel 1904 date system, date number 0 is January 1, 1904 A.D.
-        Facebook_OFR_Post$Posted <- as.character(Facebook_OFR_Post$Posted)
+        Facebook_OFR_Post$Posted <- as.Date(Facebook_OFR_Post$Posted)
         #Remove duplicate entries
         duplicate <- which(duplicated(Facebook_OFR_Post$Posted))
         Facebook_OFR_Post <- Facebook_OFR_Post[-duplicate,]
         
-        Facebook_OFR_Post$Posted <- as.Date(Facebook_OFR_Post$Posted, origin = "1900-01-01") #In the Excel 1900 date system, the Excel serial date number 1 corresponds to January 1, 1900 A.D. In the Excel 1904 date system, date number 0 is January 1, 1904 A.D.
-
         ## Set initial state with a few regions selected and a log y-axes
         myState <- '
             {"showTrails":false,
             "colorOption":"_UNIQUE_COLOR",
             "playDuration":150000}
         '
-
+        titre = paste("Performance des posts facebook: du", 
+                      format(min(Facebook_OFR_Post$Posted), format="%d %B %Y"),
+                      "au",
+                      format(max(Facebook_OFR_Post$Posted), format="%d %B %Y")
+        )
+                
         Motion <- gvisMotionChart(
             Facebook_OFR_Post, 
             idvar = "Type", 
@@ -100,34 +114,76 @@ shinyServer(function(input, output) {
         return(Motion)
     })
     
+############################ Videos insights ############################
+output$dygraph <- renderDygraph({
+    withProgress(message = 'Extraction et analyse du fichier en cours ',
+                 detail = 'Patience...', value = 0, {
+                     facebook.insight <- reactive_Facebook()
+                 })
 
-############################ bubble Plot ############################
+    titre = paste("Performance des vidéos du ", 
+                  format(min(Facebook_OFR_Post$Posted), format="%d %B %Y"),
+                  "au",
+                  format(max(Facebook_OFR_Post$Posted), format="%d %B %Y")
+    )
+    
+    facebook.insight.TimeSerie <- xts(facebook.insight[,-7], order.by = facebook.insight$Posted)
+    
+    dygraph(facebook.insight.TimeSerie[,c(
+        "Lifetime.Organic.Video.Views",                                      
+        "Lifetime.Paid.Video.Views"
+        )],
+        main = titre) %>%
+        dyHighlight(highlightSeriesOpts = list(strokeWidth = 3)) %>%
+        dySeries(name = "Lifetime.Organic.Video.Views",   label = "Lifetime Organic Video Views") %>%
+        dySeries(name = "Lifetime.Paid.Video.Views",      label = "Paid Video Views") %>%
+        dyLegend(show = "always", hideOnMouseOut = TRUE) %>%
+        dyRangeSelector(keepMouseZoom = TRUE) %>%
+        dyOptions(animatedZooms = TRUE, labelsKMB = TRUE, logscale = TRUE)
+})
+
+
+############################ Bubble Plot ############################
     output$grafics <- renderGvis({
         withProgress(message = 'Extraction et analyse du fichier',
                      detail = 'Patience...', value = 0, {
                          Facebook_OFR_Post <- reactive_Facebook()
                      })
-#         class(Facebook_OFR_Post$Posted) <- "numeric"
-#         Facebook_OFR_Post$Posted <- as.Date(Facebook_OFR_Post$Posted, origin = "1900-01-01")
-        class(Facebook_OFR_Post$Lifetime.Post.Total.Reach) <- "numeric"
-        class(Facebook_OFR_Post$Lifetime.Post.organic.reach) <- "numeric"
-        class(Facebook_OFR_Post$Lifetime.Engaged.Users) <- "numeric"
-        class(Facebook_OFR_Post$Posted) <- "numeric"
-        Facebook_OFR_Post$Posted <- as.Date(Facebook_OFR_Post$Posted, origin = "1900-01-01")-2
 
-        cat("Bubble Plot")
-        cat("\n")
-        cat(paste(sapply(Facebook_OFR_Post,class), sep="\n"))
+        Facebook_OFR_Post$Tx.d.engagement_Total         <- Facebook_OFR_Post$Lifetime.Engaged.Users / Facebook_OFR_Post$Lifetime.Post.Total.Reach
+        Facebook_OFR_Post$Tx.d.engagement_Orga          <- Facebook_OFR_Post$Lifetime.Engaged.Users / Facebook_OFR_Post$Lifetime.Post.organic.reach
+        Facebook_OFR_Post$Post.Message                  <- paste(substring(Facebook_OFR_Post$Post.Message, first = 1, last = 30), "...")
+        Facebook_OFR_Post$Lifetime.Post.Total.Reach.log <- log(Facebook_OFR_Post$Lifetime.Post.Total.Reach)
 
-        Facebook_OFR_Post$Tx.d.engagement_Total <- Facebook_OFR_Post$Lifetime.Engaged.Users / Facebook_OFR_Post$Lifetime.Post.Total.Reach
-        Facebook_OFR_Post$Tx.d.engagement_Orga  <- Facebook_OFR_Post$Lifetime.Engaged.Users / Facebook_OFR_Post$Lifetime.Post.organic.reach
-        Facebook_OFR_Post$Post.Message    <- paste(substring(Facebook_OFR_Post$Post.Message, first = 1, last = 30), "...")
+        Facebook_OFR_Post[Facebook_OFR_Post$Lifetime.Post.Paid.Reach != 0, "Paid"] <- paste(Facebook_OFR_Post[Facebook_OFR_Post$Lifetime.Post.Paid.Reach != 0,"Type"], "Paid")
+        Facebook_OFR_Post[is.na(Facebook_OFR_Post$Paid), "Paid"] <- as.character(Facebook_OFR_Post[is.na(Facebook_OFR_Post$Paid), "Type"])
+        Facebook_OFR_Post$Type <- as.factor(Facebook_OFR_Post$Paid)
 
-#         titre = paste("Orange France : Performance des posts facebook")
-        titre = paste("Orange France : Performance des posts facebook: du", 
-                      min(Facebook_OFR_Post$Posted),
+        Facebook_OFR_Post <- Facebook_OFR_Post[order(Facebook_OFR_Post$Type),]
+        
+        titre = paste("Performance des posts facebook: du", 
+                      format(min(Facebook_OFR_Post$Posted), format="%d %B %Y"),
                       "au",
-                      max(Facebook_OFR_Post$Posted)
+                      format(max(Facebook_OFR_Post$Posted), format="%d %B %Y")
+        )
+        
+        optionsList <- list(
+            title = titre,
+            hAxis = "{title: 'Total Reach', logScale: true}",
+            vAxis = "{title: 'Taux d engagement : Engaged Users / Total Reach', format:'#,###%'}",
+            width=1000, height=600, 
+            hAxis.gridlines.count = "{color: '#333', count: 2}",
+            explorer="{actions: [
+                'dragToZoom',
+                'rightClickToReset'],
+                maxZoomIn:0.05}", 
+            animation = "{
+                    duration: 2000,
+                    easing: 'out',
+                startup: 'TRUE'}",
+            bubble="{textStyle:{color: 'none'}}",
+            series= "{'Link': {color: '#00CC00'},'Link Paid': {color: '#00FF00'},'Photo': {color: 'FFCC00'},'Photo Paid': {color: '#FFFF00'},'Video': {color: '#00CCFF'},'Video Paid': {color: '#00FFFF'}}",
+            gvis.editor="Edit me!"
         )
         
         BubbleOFR_Total <- gvisBubbleChart(
@@ -137,24 +193,8 @@ shinyServer(function(input, output) {
             yvar = "Tx.d.engagement_Total",
             colorvar="Type",
             sizevar="Lifetime.Engaged.Users",
-            
-            options=list(
-                title = titre,
-                hAxis = "{title: 'Total Reach'}",
-                vAxis = "{title: 'Taux d engagement : Engaged Users / Total Reach', format:'#,###%'}",
-                width=1000, height=600, 
-                hAxis.gridlines.count = "{color: '#333', count: 2}",
-                explorer="{actions: [
-                'dragToZoom',
-                'rightClickToReset'],
-                maxZoomIn:0.05}", 
-                animation = "{
-                    duration: 2000,
-                    easing: 'out',
-                startup: 'TRUE'}",
-        
-                bubble="{textStyle:{color: 'none'}}"
-            )
+            chartid = "FacebookBubblePlot",
+            options = optionsList
         )
         return(BubbleOFR_Total)
     })
